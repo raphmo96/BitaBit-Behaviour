f1f78110f4afceb33779c6e67b7fbd82e698e5ca
jimmy <jimmygagvez@gmail.com>
2019-04-27 11:13:49 -0400

    Walker ugly model Level design begining

---
+++ BitaBit@Behaviour/Assets/Scripts/WalkerCoolFeatures/Antenna.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Antenna : MonoBehaviour
+{
+    private bool m_ShowDestination = false;
+
+    private Vector3 m_Destination = new Vector3();
+    [SerializeField]
+    private float m_RotSpeed = 1f;
+
+    private void Update()
+    {
+        if(m_ShowDestination)
+        {
+            ShowDestination();
+
+        }
+        else
+        {
+            Rotate();
+        }
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(Vector3.up, m_RotSpeed);
+    }
+
+    private void ShowDestination()
+    {
+
+    }
+
+}
---
+++ BitaBit@Behaviour/Assets/Scripts/WalkerCoolFeatures/MaterialMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialMover : MonoBehaviour
+{
+    private Renderer m_Renderer;
+    private float m_Offset;
+    [SerializeField]
+    private float m_ScrollSpeed = 0.015f;
+
+    private Vector2 m_ScrollPos = new Vector2();
+    
+
+    private void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+
+    private void Update()
+    {
+        m_Offset = Time.time * m_ScrollSpeed;
+        m_ScrollPos.x = m_Offset;
+        m_Renderer.material.SetTextureOffset("_MainTex", m_ScrollPos);
+    }
+}
