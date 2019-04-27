7a05d84d3a99588f280a9d1ccdade43d55e136a3
Raphael Morin <raph_morin23@hotmail.com>
2019-04-26 23:35:22 -0400

    Spidey control & Tmp import Input manager asset

---
+++ BitaBit@Behaviour/Assets/Scripts/Controller/SpiderController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject m_FrontRightLeg;
+    [SerializeField] private GameObject m_FrontLeftLeg;
+    [SerializeField] private GameObject m_BackRightLeg;
+    [SerializeField] private GameObject m_BackLeftLeg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
