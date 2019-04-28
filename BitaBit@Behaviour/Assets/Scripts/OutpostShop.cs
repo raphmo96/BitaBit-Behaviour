0c8cd2f6698f99234cefdc0699c5a919b2879fb1
patrenaud <pat_renaud@hotmail.com>
2019-04-27 20:58:20 -0400

    consumables

--- BitaBit@Behaviour/Assets/Scripts/OutpostShop.cs
+++ BitaBit@Behaviour/Assets/Scripts/OutpostShop.cs
@@ -26,7 +26,7 @@ public class OutpostShop : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<SpiderController>())
+        if (other.gameObject.GetComponent<FootController>())
         {
 
             PlayerManager.Instance.SpendRessources();
