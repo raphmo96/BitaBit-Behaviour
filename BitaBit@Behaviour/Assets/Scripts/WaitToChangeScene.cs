d2811d996b493ef67796d922efcf71c850521339
Magikarplvl5 <mathieu_fournier99@hotmail.fr>
2019-04-28 15:24:38 -0400

    Timeline done

---
+++ BitaBit@Behaviour/Assets/Scripts/WaitToChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitToChangeScene : MonoBehaviour
+{
+    public float m_TimeToWait = 15f;
+    public EScenes m_SceneToChange = EScenes.MainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitAndChangeScene());
+    }
+
+
+    private IEnumerator WaitAndChangeScene()
+    {
+        yield return new WaitForSeconds(m_TimeToWait);
+        SceneLoadingManager.Instance.ChangeScene(m_SceneToChange);
+    }
+}
